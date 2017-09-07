f0cc1af8f382c2dbb5a7657024632002d4f3e78c
zotov rudolph <mydream198735@aol.com>
2017-09-08 09:15:36 +1000

    complete

---
+++ src/config.js
@@ -0,0 +1,10 @@
+// Firebase
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBw2cy7z7DkQ17LDr5paxWE3P_kS3Ame8U",
+  authDomain: "chatserver-b369d.firebaseapp.com",
+  databaseURL: "https://chatserver-b369d.firebaseio.com",
+  projectId: "chatserver-b369d",
+  storageBucket: "chatserver-b369d.appspot.com",
+  messagingSenderId: "874338425418"
+};
